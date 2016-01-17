f0ff330fbcce3fae3c3f666aa73e2db53715d0c6
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2016-01-16 21:07:54 -0800

    First commit

---
+++ RoboHome/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  RoboHome
+//
+//  Created by Matthew Allen Lin on 1/16/16.
+//  Copyright © 2016 Matthew Lin. All rights reserved.
+//
+
+import UIKit
+import BluetoothKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
